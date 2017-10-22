7419d43147b4f526e51156a843ddf62e78142531
Sung <sungmkim80@gmail.com>
2017-10-21 21:41:49 -0400

    implemented.

--- ConvertMp3ToWave2/Program.cs
+++ ConvertMp3ToWave2/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NAudio.Wave;
+using System.Diagnostics;
+using System;
+using System.IO;
 
-namespace ConvertMp3ToWave2
+namespace ConvertMp3ToWav2
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			if (args.Length < 2)
+			{
+				DisplayHelp();
+				return;
+			}
+
+			Stopwatch watch = new Stopwatch();
+			watch.Start();
+
+			if (!File.Exists(args[0]))
+			{
+				Console.WriteLine($"File '{args[0]}' doesn't exist!");
+				return;
+			}
+
+			// http://markheath.net/post/converting-mp3-to-wav-with-naudio
+			using (Mp3FileReader reader = new Mp3FileReader(args[0]))
+			using (WaveStream pcmStream = WaveFormatConversionStream.CreatePcmStream(reader))
+			{
+				WaveFileWriter.CreateWaveFile(args[1], pcmStream);
+			}
+
+			watch.Stop();
+			Console.WriteLine($"It took {watch.Elapsed}");
+		}
+
+		private static void DisplayHelp()
+		{
+			Console.WriteLine("m2w.exe <input> <output>");
 		}
 	}
 }
